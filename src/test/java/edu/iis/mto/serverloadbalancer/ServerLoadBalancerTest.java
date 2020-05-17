d813cc4ff5de40ec033c80c81c9629c91dd2b5a8
belka <izaa1109@gmail.com>
2020-05-17 03:55:39 +0200

    added parameters for 7 test

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -6,8 +6,28 @@ import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class ServerLoadBalancerTest {
+	@Parameterized.Parameters
+	public static Collection<Object[]> serverAndVmValues(){
+		return Arrays.asList(new Object[][]{{10, 10, 50.0, 45.0, 2, 4, 2},
+				{5, 20, 40.0, 20.0, 6, 3, 2 }});
+	}
+
+	@Parameterized.Parameter public int capacity1;
+	@Parameterized.Parameter(1) public int capacity2;
+	@Parameterized.Parameter(2) public double load1;
+	@Parameterized.Parameter(3) public double load2;
+	@Parameterized.Parameter(4) public int size1;
+	@Parameterized.Parameter(5) public int size2;
+	@Parameterized.Parameter(6) public int size3;
+
 	@Test
 	public void itCompiles() {
 		assertThat(true, equalTo(true));
@@ -64,11 +84,11 @@ public class ServerLoadBalancerTest {
 
 	@Test
 	public void balance_serversAndVms() {
-		Server firstServer = build(server().withCapacity(10).withLoadOf(50.0));
-		Server secondServer = build(server().withCapacity(10).withLoadOf(45.0));
-		VM vm1 = build(vm().withSize(2));
-		VM vm2 = build(vm().withSize(4));
-		VM vm3 = build(vm().withSize(2));
+		Server firstServer = build(server().withCapacity(capacity1).withLoadOf(load1));
+		Server secondServer = build(server().withCapacity(capacity2).withLoadOf(load2));
+		VM vm1 = build(vm().withSize(size1));
+		VM vm2 = build(vm().withSize(size2));
+		VM vm3 = build(vm().withSize(size3));
 		balancing(listOfServers(firstServer, secondServer), listOfVms(vm1, vm2, vm3));
 		Assert.assertTrue(secondServer.contains(vm1));
 		Assert.assertTrue(firstServer.contains(vm2));
